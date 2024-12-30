fc5cc62286c0be3f96a594667e7044e4abaab2b5
Koduru Balu Harshavardan <balu121199@gmail.com>
2024-12-30 15:52:41 -0500

    Initial commit

--- app/main.py
+++ app/main.py
@@ -112,4 +112,4 @@ lambda_handler = Mangum(app)
 # # For local testing
 # if __name__ == "__main__":
 #     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)git
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
