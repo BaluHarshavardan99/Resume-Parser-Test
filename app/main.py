9f38b4a6d677fa077e87291d8323c0823c9813b2
Koduru Balu Harshavardan <balu121199@gmail.com>
2024-12-30 15:51:28 -0500

    TESTING THE GITHUB ACTIONS

--- app/main.py
+++ app/main.py
@@ -109,7 +109,7 @@ def process_pdf(file_path):
 
 lambda_handler = Mangum(app)
 
-# For local testing
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# # For local testing
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)git
